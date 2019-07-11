5bb1e5f1bad0ee7552afab45b7b11c5ed624766c
System Administrator <root@Chriss-MacBook-Pro.local>
2019-07-10 19:13:21 -0700

    adding heroku database

--- server.js
+++ server.js
@@ -14,10 +14,8 @@ const PORT = process.env.PORT || 3000;
 const db = knex({
     client: 'pg',
     connection: {
-      host : '127.0.0.1',
-      user : 'chyer',
-      password : '',
-      database : 'smart-brain'
+      connectionString: process.env.DATABASE_URL,
+      ssl: true
     }
 });
 
